a63c3e068c9618a1b33e8aa82a055471af7b082b
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-16 20:13:57 +0530

    packer

--- install-packer.sh
+++ install-packer.sh
@@ -1,3 +1,3 @@
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install packer
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install packer
